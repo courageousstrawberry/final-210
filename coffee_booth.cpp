e7df6067f78a396a2a42ac5d84356290f243dd18
courageousstrawberry <elech2a@gmail.com>
2024-12-09 19:07:46 -0800

    addNode function

--- coffee_booth.cpp
+++ coffee_booth.cpp
@@ -13,6 +13,24 @@ struct Node {
 const string names[] = {"Alicia", "Frank", "John", "Emma", "Sophia", "Liam", "Olivia", "Noah", "Ava", "James"};
 const string drinks[] = {"Cappuccino", "Mocha", "Americano", "Latte", "Espresso", "Macchiato", "Flat", "Affogato", "Cortado", "Irish"};
 
+void addNode(Node*& head, const string& name, const string& drink) {
+    Node* newNode = new Node;
+    newNode->name = names[name];
+    newNode->drinkOrder = drinks[name];
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        head = newNode;
+    }
+    else {
+        Node* temp = head;
+        while (temp->next != nullptr){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        }
+}
+
 int main() {
     int count = 0;
     Node *head = nullptr;
