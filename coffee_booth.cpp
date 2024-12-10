40221c9c1ca04774f5c2f87157c7430921fd222e
courageousstrawberry <elech2a@gmail.com>
2024-12-09 18:57:55 -0800

    generate 3 customers

--- coffee_booth.cpp
+++ coffee_booth.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -8,12 +10,26 @@ struct Node {
     Node* next;
 };
 
-const string names[] = {"Alicia", "Frank"};
-const string drinkOrders[] = {"Cappuccino", "Mocha", "Americano"};
+const string names[] = {"Alicia", "Frank", "John", "Emma", "Sophia", "Liam", "Olivia", "Noah", "Ava", "James"};
+const string drinks[] = {"Cappuccino", "Mocha", "Americano", "Latte", "Espresso", "Macchiato", "Flat", "Affogato", "Cortado", "Irish"};
 
 int main() {
     int count = 0;
+    Node *head = nullptr;
+    // Generate 3 Customers
+    for (int i = 0; i < 3; i++){
+        int randName = rand() % 10 + 1;
+        int randDrink = rand() % 10 + 1;
+
+        Node* newNode = new Node;
+        newNode->name = names[randName];
+        newNode->drinkOrder = drinks[randDrink];
+        newNode->next = nullptr;
+        head = newNode;
+    }
+
     while (count < 10) {
+        
         
         count++;
     }
